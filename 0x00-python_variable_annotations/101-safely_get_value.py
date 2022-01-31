50d5a1aef1fd7b299c9f71b4c5040585a0b3dbc7
vanemcb <vanem2201@gmail.com>
2022-01-31 17:48:48 -0500

    Task 11 created

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Type-annotated function safe_first_element"""
+from typing import Union, Any, Mapping, TypeVar
+
+T = TypeVar('T')
+
+
+def safely_get_value(
+        dct: Mapping, key: Any, default: Union[T, None]=None) -> Union[Any, T]:
+    """ Function that returns a dictionary value """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
