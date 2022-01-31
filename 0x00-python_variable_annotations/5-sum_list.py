2a011be4b20915427a643002d76ab172baff84ad
vanemcb <vanem2201@gmail.com>
2022-01-31 15:49:12 -0500

    Task 5 fixed

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ Type-annotated function sum_list """
+from typing import List
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ function that takes a list of floats as argument and
     returns  their sum as a float. """
 
