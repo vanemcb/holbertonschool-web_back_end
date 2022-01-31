df4350b10fa35bf3288efbb8d3f5fbd245326da2
vanemcb <vanem2201@gmail.com>
2022-01-31 15:14:55 -0500

    Task 2 created

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Type-annotated function floor """
+
+import math
+
+
+def floor(n: float) -> int:
+    """ function that takes a float as argument and
+    returns the floor of the float. """
+    return math.floor(n)
