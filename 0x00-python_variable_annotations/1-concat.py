61b8731f0da6e1ebe4f8240d2be73f71c9bb5922
vanemcb <vanem2201@gmail.com>
2022-01-31 15:01:01 -0500

    Task 1 created

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Type-annotated function concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """ function that takes two strings as arguments and
+    returns a concatenated string. """
+    return str1 + str2
