08904dcee8e0e580d47f649b4d6b321e65cd5c30
vanemcb <vanem2201@gmail.com>
2022-02-01 16:06:39 -0500

    Task 0 created

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Asynchronous coroutine wait_random"""
+
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ Function that takes an integer argument and
+    waits for a random delay and eventually returns it"""
+    delay = await random.randrange(0, max_delay)
+    return delay
