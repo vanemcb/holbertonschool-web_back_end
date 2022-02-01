8ffa52481efa318f5ebc06261d3ec95e36b54cab
vanemcb <vanem2201@gmail.com>
2022-01-31 19:25:27 -0500

    Task 8 fixed

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """ Type-annotated function make_multiplier """
+from audioop import mul
 from typing import List, Union, Tuple, Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """ function that takes a float and as argument
     and returns a function that multiplies a float by multipliere. """
+    def f(num: float) -> float:
+        return num * multiplier
 
-    return make_multiplier(f(num, multiplier))
+    return f
