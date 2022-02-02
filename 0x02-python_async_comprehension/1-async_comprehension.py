7dca4dad20b90b3ebcff065bd3091aa5f7aed575
vanemcb <vanem2201@gmail.com>
2022-02-02 17:21:04 -0500

    Task 2 created

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Coroutine called async_generator """
+""" Coroutine called async_comprehension """
 from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Coroutine called measure_runtime """
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+import asyncio
+from time import perf_counter
+
+
+async def measure_runtime() -> float:
+    """ Function that measure the total runtime """
+    t1 = perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                            async_comprehension(), async_comprehension(),)
+    t2 = perf_counter()
+    return t2 - t1
