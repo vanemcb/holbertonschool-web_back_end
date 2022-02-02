48de9f1a9c05ce4a499df84200c0837634462c4f
vanemcb <vanem2201@gmail.com>
2022-02-02 17:25:53 -0500

    Style fixed

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ Coroutine called measure_runtime """
-async_comprehension = __import__('1-async_comprehension').async_comprehension
 import asyncio
 from time import perf_counter
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
