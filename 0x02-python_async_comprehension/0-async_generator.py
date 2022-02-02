d3d7d54ad44e3f81348afd3902f572ffc8140a3c
vanemcb <vanem2201@gmail.com>
2022-02-02 17:13:42 -0500

    Task 0 fixed

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """ Coroutine called async_generator """
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """ Coroutine that loops 10 times, each
     time asynchronously wait 1 second """
     for i in range(10):
