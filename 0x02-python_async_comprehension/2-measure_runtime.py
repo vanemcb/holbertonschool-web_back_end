ecaa60c7fd1b846efe57fe2e736f734a80ea3071
vanemcb <vanem2201@gmail.com>
2022-02-02 17:22:25 -0500

    Style fixed

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -9,6 +9,6 @@ async def measure_runtime() -> float:
     """ Function that measure the total runtime """
     t1 = perf_counter()
     await asyncio.gather(async_comprehension(), async_comprehension(),
-                            async_comprehension(), async_comprehension(),)
+                         async_comprehension(), async_comprehension(),)
     t2 = perf_counter()
     return t2 - t1
