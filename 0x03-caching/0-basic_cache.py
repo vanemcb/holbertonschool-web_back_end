6eb8399fc42297382e4539569ce80e25a545ac21
vanemcb <vanem2201@gmail.com>
2022-02-04 12:44:50 -0500

    Function comments added

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -7,11 +7,12 @@ BaseCaching = __import__('base_caching').BaseCaching
 class BasicCache(BaseCaching):
     """ Class BasicCache  """
     def put(self, key, item):
+        """ Function put """
         if key != None and item != None:
             self.cache_data[key] = item
 
     def get(self, key):
-
+        """ Function get """
         if key in self.cache_data and key != None:
             return(self.cache_data[key])
         else:
