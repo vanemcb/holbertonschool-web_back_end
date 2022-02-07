1cc0b93a63f0ae13b58ab9fce6f1d77743a4d7ca
vanemcb <vanem2201@gmail.com>
2022-02-07 15:50:03 -0500

    Task 1 created

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" FIFOCache module
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+
+    def put(self, key, item):
+        """ Function put """
+
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                array_key = sorted(self.cache_data.keys())
+                print("DISCARD: {}".format(array_key[0]))
+                self.cache_data.pop(array_key[0])
+
+    def get(self, key):
+        """ Function get """
+        if key in self.cache_data and key is not None:
+            return(self.cache_data[key])
+        else:
+            return None
