cb4f6961566c0f19f565dd6c1c81d71082f014e7
vanemcb <vanem2201@gmail.com>
2022-02-11 17:01:42 -0500

    Task 1 created

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,6 +2,29 @@
 """ Module function filter_datum """
 from typing import List
 import re
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        test_dict = {'name': record.name,
+                     'levelname': record.levelname,
+                     'asctime': record.asctime,
+                     'message': record.msg}
+        return self.FORMAT % test_dict
 
 
 def filter_datum(
