32465848e3ebe29fcc5b5a870805d6bf6ea028e6
vanemcb <vanem2201@gmail.com>
2022-02-11 13:49:19 -0500

    Style fixed

--- 0x04-pagination/1-simple_pagination.py
+++ 0x04-pagination/1-simple_pagination.py
@@ -32,11 +32,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """ Function that returns the range of rows """
-            assert(type(page) == int and page > 0)
-            assert(type(page_size) == int and page_size > 0)
-            index, end_index = index_range(page, page_size)
-            array_data = self.dataset()
-            if index > len(array_data):
-                return []
-            return array_data[index:end_index]
+        """ Function that returns the range of rows """
+        assert(type(page) == int and page > 0)
+        assert(type(page_size) == int and page_size > 0)
+        index, end_index = index_range(page, page_size)
+        array_data = self.dataset()
+        if index > len(array_data):
+            return []
+        return array_data[index:end_index]
