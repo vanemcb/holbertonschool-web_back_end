77b4bc66edc9ede579d00bf7e4c480edd55bc9f8
vanemcb <vanem2201@gmail.com>
2022-02-13 16:09:59 -0500

    Task 6 created

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -11,3 +11,12 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Function that validates if the provided password
+    matches the hashed password """
+
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password):
+        return True
+    return False
