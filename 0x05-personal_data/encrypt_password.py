df8d36da61c5a0bf6595cf12a2db8591cc40a877
vanemcb <vanem2201@gmail.com>
2022-02-13 15:59:26 -0500

    Return bytes

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -3,7 +3,7 @@
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """ Function that returns a salted, hashed password,
     which is a byte string """
 
