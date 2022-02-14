18254d962e29f9c1f7a5415c49f4588dffa00147
vanemcb <vanem2201@gmail.com>
2022-02-14 13:55:18 -0500

    Task 4 created

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,17 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Public method require_auth """
-        return False
+
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path[len(path) - 1] != '/':
+            path = path + '/'
+
+        if path in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ Public method authorization_header """
