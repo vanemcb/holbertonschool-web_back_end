4a500f55ddac8be7bd07fce445421357339fdc8f
vanemcb <vanem2201@gmail.com>
2022-02-14 11:09:14 -0500

    Task 1 created

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -21,7 +21,14 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+@app.errorhandler(401)
+def unauthorized_req(error) -> str:
+    """ Request unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
