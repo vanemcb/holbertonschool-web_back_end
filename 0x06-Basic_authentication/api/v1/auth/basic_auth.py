015426ac11a9b3510d0cb07e5a246a89c7a6f3b2
vanemcb <vanem2201@gmail.com>
2022-02-15 17:08:29 -0500

    Task 8 created

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 Module BasicAuth class
 """
+from base64 import b64decode
 from api.v1.auth.auth import Auth
 
 
@@ -20,3 +21,21 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """ Returns the decoded value of a Base64 string
+        base64_authorization_header """
+
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+
+        try:
+            base_encode = base64_authorization_header.encode('utf-8')
+            base_decode64 = b64decode(base_encode)
+            decoded = base_decode64.decode('utf-8')
+            return decoded
+        except Exception:
+            return None
