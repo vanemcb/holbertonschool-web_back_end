b9a99fd317af651ff418485923db92e2a38c34bc
vanemcb <vanem2201@gmail.com>
2022-02-17 10:36:23 -0500

    Task 0 fixed

--- 0x07-Session_authentication/api/v1/views/users.py
+++ 0x07-Session_authentication/api/v1/views/users.py
@@ -30,10 +30,12 @@ def view_one_user(user_id: str = None) -> str:
     if user_id == 'me' and request.current_user is None:
         abort(404)
     if user_id == 'me' and request.current_user is not None:
-        user = User.get(user_id)
-        if user is None:
-            abort(404)
-        return jsonify(user.to_json())
+        return jsonify(request.current_user.to_json())
+
+    user = User.get(user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
