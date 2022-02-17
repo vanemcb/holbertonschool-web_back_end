067cb65a6f03615ab259d0481a31addb92997eba
vanemcb <vanem2201@gmail.com>
2022-02-17 14:17:07 -0500

    Task 4 created

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ Module Auth class
 """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth():
@@ -36,3 +37,13 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """ Public method current_user """
         return None
+
+    def session_cookie(self, request=None):
+        """ Method that returns a cookie value from a request """
+
+        if request is None:
+            return None
+
+        _my_session_id = getenv('SESSION_NAME')
+        value_cookie = request.cookies.get(_my_session_id)
+        return value_cookie
