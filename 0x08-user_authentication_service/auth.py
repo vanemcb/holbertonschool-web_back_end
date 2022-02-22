9cdce509d91190627c3581ac69c2393fbdff8fcc
vanemcb <vanem2201@gmail.com>
2022-02-22 15:54:28 -0500

    Task 5 created

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -2,7 +2,25 @@
 """Auth module
 """
 import bcrypt
+from db import DB
+from user import User
 
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Method that checks if a user already exists
+        """
+        user = self._db.find_user_by(email=email)
+        if user is not None:
+            raise ValueError('User {} already exists'.format(email))
+        passw = _hash_password(password)
+        return self._db.add_user(email, passw)
 
 def _hash_password(password: str) -> bytes:
     """ Function that returns a salted, hashed password,
