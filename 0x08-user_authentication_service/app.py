e6078e9d8c25e2d97d79b798ba35d629893a5d48
vanemcb <vanem2201@gmail.com>
2022-02-23 12:17:58 -0500

    Test task 1

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -3,6 +3,10 @@
 Route module for the API
 """
 from flask import jsonify
+from auth import Auth
+
+
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
@@ -10,6 +14,23 @@ def message() -> str:
     """ Main route
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def create_user(email: str, password: str) -> str:
+    """ POST users/
+    JSON body:
+      - email
+      - password
+    Return:
+      - User object JSON represented
+      - 400 if can't create the new User
+    """
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    ex
+
+
 
 
 if __name__ == "__main__":
