9ea3eb5c3b088f06aa7c33266e7356f3cad913ca
vanemcb <vanem2201@gmail.com>
2022-02-24 18:24:25 -0500

    Task 15 created

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -59,5 +59,17 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profilet():
+    """ Method that looks for user by session ID
+    """
+    session_id = request.cookies.get('session_id')
+    try:
+        user = AUTH.get_user_from_session_id(session_id)
+        return jsonify({"email": user.email}), 200
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
