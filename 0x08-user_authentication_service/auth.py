28aeb369f9b0638a0b50fb9d3fdf9e5fb5aeb97a
vanemcb <vanem2201@gmail.com>
2022-02-24 17:48:35 -0500

    Task 12 created

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -47,6 +47,15 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Method that returns the user by session
+        """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except Exception:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """Function that returns a salted, hashed password,
