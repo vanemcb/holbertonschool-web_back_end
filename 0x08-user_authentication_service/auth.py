77bb12974b63a4241e012e52f6ae22c04004ed80
vanemcb <vanem2201@gmail.com>
2022-02-24 11:23:51 -0500

    Task 8 created

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -29,11 +29,12 @@ class Auth:
         the password
         """
         try:
-            self._db.find_user_by(email=email)
-        except Exception:
-            if bcrypt.checkpw(password, _hash_password(password)):
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode(
+                    'utf-8'), user.hashed_password):
                 return True
-        return False
+        except NoResultFound:
+            return False
 
 
 def _hash_password(password: str) -> bytes:
