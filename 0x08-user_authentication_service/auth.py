be44ceb9e87d3a16c76fbe93273a7c0cff0cc130
vanemcb <vanem2201@gmail.com>
2022-02-25 15:15:59 -0500

    Task 16 created

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -63,6 +63,15 @@ class Auth:
         user = self._db.find_user_by(id=user_id)
         user.session_id = None
 
+    def get_reset_password_token(self, email: str) -> str:
+        """Method that updates the user's reset_token database field
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            user.reset_token = str(uuid4())
+        except NoResultFound:
+            raise ValueError
+
 
 def _hash_password(password: str) -> bytes:
     """Function that returns a salted, hashed password,
