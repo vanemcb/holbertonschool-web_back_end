f2fb8140d596f7601d85320f2b5d51150f0763b3
vanemcb <vanem2201@gmail.com>
2022-03-01 16:34:05 -0500

    Task 2 created

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -2,9 +2,10 @@
 """Test utils module
 """
 from unittest import TestCase
+from unittest.mock import patch
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(TestCase):
@@ -28,6 +29,27 @@ class TestAccessNestedMap(TestCase):
         """Method that tests if a KeyError is raised
         """
         self.assertRaises(KeyError, access_nested_map, nested_map, path)
+
+
+class TestGetJson(TestCase):
+    """Class TestGetJson
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, url, test_payload):
+        """Method that tests utils.get_json
+        """
+        class Mocked(Mock):
+            """Mocked class"""
+            def json(self):
+                """json method mocked"""
+                return test_payload
+
+        with patch('requests.get') as MockClass:
+            MockClass.return_value = Mocked()
+            self.assertEqual(get_json(url), test_payload)
 
 
 if __name__ == '__main__':
