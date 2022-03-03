fc709356bebf9330e025236e471e8731cdd67305
vanemcb <vanem2201@gmail.com>
2022-03-03 17:00:22 -0500

    Task 2 created

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Module basic Flask app
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """Class Config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world():
+    """Index page"""
+    return render_template('0-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """Method that determines the best match with
+    our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
