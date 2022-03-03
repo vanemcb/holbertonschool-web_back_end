184b9bb9b89ceffff67eea7037a2f3e04208d13c
vanemcb <vanem2201@gmail.com>
2022-03-03 15:47:23 -0500

    Task 0 fixed

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -2,12 +2,17 @@
 """
 Module basic Flask app
 """
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
+def hello_world():
+    """Index page"""
+    return render_template('0-index.html')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
