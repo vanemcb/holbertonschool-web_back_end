c0db21140e1cb835001395919b9f650ee5b495ed
vanemcb <vanem2201@gmail.com>
2022-03-15 14:26:57 -0500

    task 0 fixed

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -15,7 +15,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method that generates a random key, stores the input data in Redis
         using the random key and return the key.
         """
