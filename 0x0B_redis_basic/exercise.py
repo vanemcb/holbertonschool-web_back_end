20277c43b904b00f8a9ab8597f0d3f45c3f2ba60
vanemcb <vanem2201@gmail.com>
2022-03-15 17:24:23 -0500

    Task 3 created

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -14,12 +14,29 @@ def count_calls(method: Callable) -> Callable:
     key = method.__qualname__
 
     @wraps(method)
-    def wrapper(self, *args, **kwds):
+    def wrapper(self, *args, **kwargs):
         """Function that increments the count for that key every time the
         method is called and returns the value returned by the original method.
         """
         self._redis.incr(key)
-        return method(self, *args, **kwds)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """Decorator to store the history of inputs and outputs for a particular
+    function.
+    """
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """wrapped function
+        """
+        input = str(args)
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
     return wrapper
 
 
@@ -33,6 +50,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method that generates a random key, stores the input data in Redis
         using the random key and return the key.
