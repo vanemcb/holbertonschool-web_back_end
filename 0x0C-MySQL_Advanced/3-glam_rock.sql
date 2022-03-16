f2bc8e5e366109ce7ef18f28112bbe8b2a83fbae
vanemcb <vanem2201@gmail.com>
2022-03-16 16:22:31 -0500

    Task 3 created

---
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,4 @@
+-- script that lists all bands with Glam rock as their main style, ranked by
+-- their longevity
+
+SELECT band_name,(split - formed) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
