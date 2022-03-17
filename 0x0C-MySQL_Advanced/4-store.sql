d77ef6c4a0f524b5ecd5c9fe4cf582c914de0f89
vanemcb <vanem2201@gmail.com>
2022-03-17 11:32:26 -0500

    Use NEW

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -4,4 +4,4 @@
 CREATE TRIGGER my_trigger
 AFTER INSERT
 ON orders FOR EACH ROW
-UPDATE items SET quantity = quantity - 1;
+UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
