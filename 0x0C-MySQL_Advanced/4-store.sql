b7951681f782c957c8b662c98382828472ad237a
vanemcb <vanem2201@gmail.com>
2022-03-17 11:27:49 -0500

    Task 4 created

--- 0x0C-MySQL_Advanced/4-store.sql
+++ 0x0C-MySQL_Advanced/4-store.sql
@@ -1,4 +1,7 @@
 --  script that creates a trigger that decreases the quantity of an item after
 -- adding a new order.
 
-CREATE TRIGGER my_trigger AFTER INSERT ON orders
+CREATE TRIGGER my_trigger
+AFTER INSERT
+ON orders FOR EACH ROW
+UPDATE items SET quantity = quantity - 1;
