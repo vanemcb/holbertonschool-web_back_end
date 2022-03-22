5b588a8ebd71c252d4fd80cd637e243c0cbe90ce
vanemcb <vanem2201@gmail.com>
2022-03-22 16:54:22 -0500

    Updating task 15

---
+++ 0x0D-NoSQL/102-log_stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Module task 15
+"""
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    count_docs = nginx_collection.count_documents({})
+    count_get = nginx_collection.count_documents({'method': 'GET'})
+    count_post = nginx_collection.count_documents({'method': 'POST'})
+    count_put = nginx_collection.count_documents({'method': 'PUT'})
+    count_patch = nginx_collection.count_documents({'method': 'PATCH'})
+    count_del = nginx_collection.count_documents({'method': 'DELETE'})
+    count_path = nginx_collection.count_documents({'path': '/status'})
+
+    dict_methods = {'GET': count_get,
+                    'POST': count_post,
+                    'PUT': count_put,
+                    'PATCH': count_patch,
+                    'DELETE': count_del
+                    }
+
+    dict_ips = {}
+    for doc in nginx_collection.find({}):
+        dict_ips[doc['ip']] = 0
+
+    print('{} logs\nMethods:'.format(count_docs))
+    for key, value in dict_methods.items():
+        print('\tmethod {}: {}'.format(key, value))
+    print('{} status check'.format(count_path))
+
+    print(dict_ips)
