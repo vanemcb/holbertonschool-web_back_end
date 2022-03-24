92e069a7b50769fbe15c848e0410c1574745a9c9
vanemcb <vanem2201@gmail.com>
2022-03-24 12:05:43 -0500

    let -> const

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
-  for (let item of array) {
+  for (const item of array) {
     array[idx] = appendString + item;
     idx++;
   }
