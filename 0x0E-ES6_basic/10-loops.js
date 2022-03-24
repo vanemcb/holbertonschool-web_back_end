241080eb3dd1893819bd71ad17b0b0ae76e5ac5b
vanemcb <vanem2201@gmail.com>
2022-03-24 12:15:37 -0500

    ESLint desable

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   let idx = 0;
   for (const item of array) {
-    array[idx] = appendString + item;
-    idx++;
+    array[idx] = appendString + item;// eslint-disable-line no-param-reassign
+    idx += 1;
   }
 
   return array;
