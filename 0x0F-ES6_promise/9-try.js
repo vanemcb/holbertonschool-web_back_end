8d392296a383664a725db1ecb694fb2c41d7327b
vanemcb <vanem2201@gmail.com>
2022-03-29 15:51:02 -0500

    Task 9 created

---
+++ 0x0F-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  let result;
+  try {
+    result = mathFunction();
+  } catch (err) {
+    result = err.toString();
+  }
+  queue.push(result);
+  queue.push('Guardrail was processed');
+  return queue;
+}
