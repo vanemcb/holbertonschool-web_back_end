f0a8a809706b03c09e8b29b593f44f53fd9eedb1
vanemcb <vanem2201@gmail.com>
2022-03-29 15:15:39 -0500

    Task 6 created

---
+++ 0x0F-ES6_promise/6-final-user.js
@@ -0,0 +1,6 @@
+import signUpUser from "./4-user-promise.js";
+import uploadPhoto from "./5-photo-reject.js";
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+
+}
