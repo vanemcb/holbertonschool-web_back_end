144fffba7d2969ab659b5a187109b64356b70cc6
vanemcb <vanem2201@gmail.com>
2022-03-29 11:10:39 -0500

    Task 2 fixed

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   const result = promise
     .then(() => ({ status: 200, body: 'Success' }))
-    .catch(() => Error(''));
+    .catch(() => new Error(''));
   console.log('Got a response from the API');
   return result;
 }
