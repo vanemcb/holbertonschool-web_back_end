f15d07a369748d62d1ff0fcfeff9ab33d7193d57
vanemcb <vanem2201@gmail.com>
2022-03-29 10:54:21 -0500

    Task 2 created

---
+++ 0x0F-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error());
+    }
+  }).then(() => console.log('Got a response from the API'));
+}
