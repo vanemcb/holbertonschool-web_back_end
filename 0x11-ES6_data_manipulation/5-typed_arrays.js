44ee1582741cbbe3e8c2d7618f135ee5e307bb47
vanemcb <vanem2201@gmail.com>
2022-04-05 15:24:31 -0500

    Task 5 modified

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = ArrayBuffer(length);
-  const typedArray = Int8Array(buffer);
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer, 0);
+  view.setInt8(position, value);
+  return view;
 }
