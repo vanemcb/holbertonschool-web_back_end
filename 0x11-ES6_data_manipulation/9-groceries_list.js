1c14792c4238b30887f469cadbd2b9b0336d958e
vanemcb <vanem2201@gmail.com>
2022-04-05 15:59:29 -0500

    Task 9 created

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const mapGroceries = new Map();
+  mapGroceries.set('Apples', 10);
+  mapGroceries.set('Tomatoes', 10);
+  mapGroceries.set('Pasta', 1);
+  mapGroceries.set('Rice', 1);
+  mapGroceries.set('Banana', 5);
+  return mapGroceries;
+}
