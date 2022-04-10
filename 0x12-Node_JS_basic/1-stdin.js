156f1b977b8c643b5dc2ee1a2b8d026aa6666983
vanemcb <vanem2201@gmail.com>
2022-04-10 11:01:31 -0500

    Task 1 modified

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
-console.log('Welcome to Holberton School, what is your name?');
+console.log('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name !== null) {
     process.stdout.write(`Your name is: ${name}`);
   }
 });
-process.on('exit', () => {
-  process.stdout.write('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
