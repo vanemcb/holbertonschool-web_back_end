c9ad7d56ebdeeac52cefdf11287b7873ef26f8e6
vanemcb <vanem2201@gmail.com>
2022-04-17 21:45:18 -0500

    Task 4 created

---
+++ 0x12-Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const port = 1245;
+const app = http.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
+});
+app.listen(port);
+module.exports = app;
