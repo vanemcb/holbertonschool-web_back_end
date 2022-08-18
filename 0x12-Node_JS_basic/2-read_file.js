df72113e3e428292fb17c82140d0268a0a371a8f
vanemcb <vanem2201@gmail.com>
2022-08-18 10:51:51 -0500

    Task 2 fixed

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -1,26 +1,19 @@
 const fs = require('fs');
 
-const countStudents = (path) => {
+const countStudents = (aPath) => {
   try {
-    const arrayLines = fs.readFileSync(path, { encoding: 'utf8' }).split('\n');
-    arrayLines.shift();
-    const arrayData = arrayLines.map((row) => row.split(','));
-    const dictField = {};
-
-    for (const field of arrayData) {
-      const keys = Object.keys(dictField);
-      if (keys.includes(field[3])) {
-        dictField[field[3]].push(field[0]);
-      } else {
-        dictField[field[3]] = [field[0]];
-      }
+    let data = fs.readFileSync(aPath, 'utf8').toString().split('\n');
+    data = data.slice(1, data.length - 1);
+    console.log(`Number of students: ${data.length}`);
+    const subjects = {};
+    for (const row of data) {
+      const student = row.split(',');
+      if (!subjects[student[3]]) subjects[student[3]] = [];
+      subjects[student[3]].push(student[0]);
     }
-
-    console.log(`Number of students: ${arrayLines.length}`);
-    const keysStr = Object.keys(dictField);
-    keysStr.forEach((item) => {
-      console.log(`Number of students in ${item}: ${dictField[item].length}. List: ${dictField[item].join(', ')}`);
-    });
+    for (const subject in subjects) {
+      if (subject) console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
+    }
   } catch (error) {
     throw new Error('Cannot load the database');
   }
